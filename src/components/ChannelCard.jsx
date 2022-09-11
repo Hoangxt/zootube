c70221ef4754805ff9918d54c4c9ff606582fa2a
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-09-11 23:23:06 +0700

    fix Profile page

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, CardContent, CardMedia, Typography } from "@mui/material";
-import { CheckCircle } from "@mui/icons-material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
@@ -42,22 +42,16 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
           }}
         />
         <Typography variant="h6">
-          {channelDetail?.statistics?.subscriberCount && (
-            <Typography
-              sx={{ fontSize: "15px", fontWeight: 500, color: "gray" }}
-            >
-              {parseInt(
-                channelDetail?.statistics?.subscriberCount
-              ).toLocaleString("en-US")}{" "}
-              Subscribers
-            </Typography>
-          )}
+          {channelDetail?.snippet?.title}{" "}
+          <CheckCircleIcon
+            sx={{ fontSize: "14px", color: "gray", ml: "5px" }}
+          />
         </Typography>
-        {channelDetail?.statistics?.subscribeCount && (
-          <Typography>
+        {channelDetail?.statistics?.subscriberCount && (
+          <Typography sx={{ fontSize: "15px", fontWeight: 500, color: "gray" }}>
             {parseInt(
-              channelDetail?.statistics?.subscribeCount
-            ).toLocaleString()}{" "}
+              channelDetail?.statistics?.subscriberCount
+            ).toLocaleString("en-US")}
             Subscribers
           </Typography>
         )}
