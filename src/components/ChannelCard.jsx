c0146ea4abaf0fd0eb80e630bc000183f45b489d
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-09-11 23:09:49 +0700

    Profile page

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail }) => (
+const ChannelCard = ({ channelDetail, marginTop }) => (
   <Box
     sx={{
       boxShadow: "none",
@@ -15,6 +15,7 @@ const ChannelCard = ({ channelDetail }) => (
       width: { sx: "356px", md: "320px" },
       height: "326px",
       margin: "auto",
+      marginTop,
     }}
   >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
@@ -41,14 +42,16 @@ const ChannelCard = ({ channelDetail }) => (
           }}
         />
         <Typography variant="h6">
-          {channelDetail?.snippet?.title || "Channel Name"}
-          <CheckCircle
-            sx={{
-              fontSize: 14,
-              color: "gray",
-              ml: "5px",
-            }}
-          />
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography
+              sx={{ fontSize: "15px", fontWeight: 500, color: "gray" }}
+            >
+              {parseInt(
+                channelDetail?.statistics?.subscriberCount
+              ).toLocaleString("en-US")}{" "}
+              Subscribers
+            </Typography>
+          )}
         </Typography>
         {channelDetail?.statistics?.subscribeCount && (
           <Typography>
